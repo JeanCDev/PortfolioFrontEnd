e7659de90b2b0786837a43cf4f212fd367ecc209
JeanCDev <jeangames15@gmail.com>
2020-12-09 17:57:01 -0300

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {FormEvent, useState} from 'react';
+import api from './api';
+
+/////////////////////////////////////////////
+/// TESTING THE CONNECTION TO THE BACKEND ///
+/////////////////////////////////////////////
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [photo, setPhoto] = useState('');
+
+  async function handleSubmit(event: FormEvent){
+
+    event.preventDefault();
+
+    await api.post('login', {
+      name,
+      password,
+      email,
+      photo
+    }).then((response =>{
+      console.log(response.data);
+    }));
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={name} onChange={e => setName(e.target.value)}/>
+        <input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+        <input type="text" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        <button type="submit">Vai</button>
+      </form>
     </div>
   );
 }
